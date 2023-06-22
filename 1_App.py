494d9be42d9efcf8a5dbc8bbdf415efaa78c6f75
towardsinnovationlab <126333471+towardsinnovationlab@users.noreply.github.com>
2023-06-22 06:29:48 +0200

    Add files via upload

---
+++ 1_App.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("# Customer Lifetime Value Prediction & Segmentation")
+         
+st.markdown("""
+        **👈 Select a page from the dropdown on the left** to see some applications of the app!""")
+
+
+#st.image("./images/natural-disaster.png", width=500)
+#st.write("Pixabay")
